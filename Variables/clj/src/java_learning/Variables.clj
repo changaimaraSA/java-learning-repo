a5bd53ceb6f0c67986afb2cf28233e75ce6d20db
Changa Imara <changa.imara@sonomaacademy.org>
2019-12-29 14:55:58 -0800

    completed Variables in cljoure

--- Variables/clj/src/java_learning/Variables.clj
+++ Variables/clj/src/java_learning/Variables.clj
@@ -1,17 +1,25 @@
 (ns java-learning.Variables
-  (:gen-class))
+ (:gen-class
+  :prefix "-"))
+  
 
-(def my-class-var (atom "class or static variable"))
+
+(create-ns 'java-learning.Variables.StaticVarExample)
+(intern 'java-learning.Variables.StaticVarExample 'my-class-var (atom "class or static variable"))
+
+
 
 (defn -main
   [& args]
-  (println @java-learning.Variables/my-class-var)
-  (reset! java-learning.Variables/my-class-var "changed text")
-  (println @java-learning.Variables/my-class-var))
+  (println @java-learning.Variables.StaticVarExample/my-class-var)
+  (reset! java-learning.Variables.StaticVarExample/my-class-var "changed text")
+  (println @java-learning.Variables.StaticVarExample/my-class-var))
+
+
 
 ; in the repl:
 ;    (compile 'java-learning.Variables)
-;    (import (java_learning Variables))
+;    (import (java-learning.Variables StaticVarExample))
 ;    (use 'clojure.reflect 'clojure.pprint)
 ;    (reflect java_learning.Variables)  ;;shows methods and the flags of a java class. very informative
 ;    (java-learning.Variables/main (make-array java.lang.String 0))
